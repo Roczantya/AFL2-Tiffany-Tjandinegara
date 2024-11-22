bef8be1ec7bbd0e20c1ffdb5c0a83927d84c942e
Roczantya <Galacthfansmo0nar45@gmail.com>
2024-11-23 02:42:54 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'], // Pastikan path sesuai
+      input: ('resources/css/app.css'), // Pastikan path sesuai
       refresh: true,
     }),
   ],
