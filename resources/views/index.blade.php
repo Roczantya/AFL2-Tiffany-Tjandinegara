a620fbcc4ec5e3ab2a578d64845af1e2bcb0f6cd
Roczantya <Galacthfansmo0nar45@gmail.com>
2024-11-23 00:49:51 +0800

    perbaiki css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,8 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Stylesheet dan Font Awesome -->
-    @vite('resources/css/style.css')
-    @vite('resources/js/main.js')
+    @vite('public/css/style.css')
+    @vite('public/js/main.js')
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
